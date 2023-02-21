0d7e56679816cb3ecb8c59f0baef308d38f69732
Ivan <pinjamar@gmail.com>
2023-02-21 18:05:19 +0100

    battleships-init

---
+++ battleships/app.js
@@ -0,0 +1,20 @@
+const optionContainer = document.querySelector(".option-container");
+const flipButton = document.querySelector("#flip-button");
+
+let angle = 0;
+
+function flip() {
+  const optionShips = Array.from(optionContainer.children);
+
+  if (angle === 0) {
+    angle = 90;
+  } else {
+    angle = 0;
+  }
+
+  optionShips.forEach((optionShip) => {
+    optionShip.style.transform = `rotate(${angle}deg)`;
+  });
+}
+
+flipButton.addEventListener("click", flip);
