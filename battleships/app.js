a11822f625f6c2b975e4944cd971d62d18f77712
Ivan <pinjamar@gmail.com>
2023-02-22 17:00:50 +0100

    adding gameboard elements and ships

--- battleships/app.js
+++ battleships/app.js
@@ -1,6 +1,8 @@
 const optionContainer = document.querySelector(".option-container");
 const flipButton = document.querySelector("#flip-button");
+const gamesBoardContainer = document.querySelector("#gamesboard-container");
 
+//option choosing
 let angle = 0;
 
 function flip() {
@@ -18,3 +20,69 @@ function flip() {
 }
 
 flipButton.addEventListener("click", flip);
+
+//creating boards
+
+const width = 10;
+
+function createBoard(color, user) {
+  const gameBoardContainer = document.createElement("div");
+
+  gameBoardContainer.classList.add("game-board");
+
+  gameBoardContainer.style.backgroundColor = color;
+
+  gameBoardContainer.id = user;
+
+  for (let i = 0; i < width * width; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.id = i;
+    gameBoardContainer.append(block);
+  }
+
+  gamesBoardContainer.append(gameBoardContainer);
+}
+
+createBoard("yellow", "player");
+createBoard("pink", "computer");
+
+//creating ships
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+  }
+}
+
+const destroyer = new Ship("destroyer", 2);
+const submarine = new Ship("submarine", 3);
+const cruiser = new Ship("cruiser", 3);
+const battleship = new Ship("battleship", 4);
+const carrier = new Ship("carier", 5);
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
+
+function addShipPiece(ship) {
+  const allBoardBlocks = document.querySelectorAll("#computer div");
+  let randomBoolean = Math.random() < 0.5;
+  let isHorizontal = randomBoolean;
+  let randomStartIndex = Math.floor(Math.random() * width * width);
+
+  let shipBlocks = [];
+
+  for (let i = 0; i < ship.length; i++) {
+    if (isHorizontal) {
+      shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+    } else {
+      shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width]);
+    }
+  }
+
+  shipBlocks.forEach((shipBlock) => {
+    shipBlock.classList.add(ship.name);
+    shipBlock.classList.add("taken");
+  });
+}
+
+ships.forEach((ship) => addShipPiece(ship));
